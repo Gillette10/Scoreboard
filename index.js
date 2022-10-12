99f2279144ea02f126db69debc7ff1abadfceb89
Gillette Aziabah <vgil.aziabah@gmail.com>
2022-10-12 09:58:50 +0000

    Final commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let count = 0
+
+let countEL = document.getElementById ("count-el")
+
+
+function add1Point() {
+    count += 1
+    countEL.textContent = count 
+}
+function add2Point() {
+    count += 2
+    countEL.textContent = count 
+}
+function add3Point() {
+    count += 3
+    countEL.textContent = count 
+}
+
+
+ let number = 0
+ 
+let countEll = document.getElementById ("count-ell")
+
+function add1() {
+    number += 1
+    countEll.textContent = number 
+}
+function add2() {
+    number += 2
+    countEll.textContent = number 
+}
+function add3() {
+    number += 3
+    countEll.textContent = number
+}
